f57cdebdade39bd88bbdad619897c147ddb34ae1
Ben Chen <bschen@yahoo-inc.com>
2015-05-10 13:50:09 -0700

    Added cheatsheet for seeing morse code within the app

--- app/src/main/java/com/stupid/hipchat/MainActivity.java
+++ app/src/main/java/com/stupid/hipchat/MainActivity.java
@@ -2,16 +2,49 @@ package com.stupid.hipchat;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ImageView;
 
-import com.stupid.hipchat.R;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    SlidingUpPanelLayout mSlidingLayout;
+    ImageView mCheatArrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        mSlidingLayout = (SlidingUpPanelLayout) findViewById(R.id.main_container);
+        mCheatArrow = (ImageView) findViewById(R.id.main_cheatsheet_arrow);
+
+        mSlidingLayout.setPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
+            @Override
+            public void onPanelSlide(View view, float v) {
+                mCheatArrow.setRotation(180 * v);
+            }
+
+            @Override
+            public void onPanelCollapsed(View view) {
+            }
+
+            @Override
+            public void onPanelExpanded(View view) {
+            }
+
+            @Override
+            public void onPanelAnchored(View view) {
+
+            }
+
+            @Override
+            public void onPanelHidden(View view) {
+
+            }
+        });
     }
 }
