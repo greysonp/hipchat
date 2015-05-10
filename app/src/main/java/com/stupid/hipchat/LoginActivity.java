0ad10ce18af802ff5d3fed1d405fab3e3f6b6460
Ben Chen <bschen@yahoo-inc.com>
2015-05-09 21:53:18 -0700

    Awesome-fying login screen

--- app/src/main/java/com/stupid/hipchat/LoginActivity.java
+++ app/src/main/java/com/stupid/hipchat/LoginActivity.java
@@ -2,19 +2,22 @@ package com.stupid.hipchat;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
+import com.stupid.hipchat.view.TextureVideoView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -27,11 +30,12 @@ import java.net.URISyntaxException;
  */
 public class LoginActivity extends Activity {
 
+    private TextureVideoView mTextureVideoView;
+    private TextView mLoginTitle;
     private EditText mUsernameView;
-
     private String mUsername;
-
     private Socket mSocket;
+
     {
         try {
             mSocket = IO.socket("http://hipsdontlie.herokuapp.com");
@@ -44,6 +48,17 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Set up video
+        mTextureVideoView = (TextureVideoView) findViewById(R.id.login_video);
+        mTextureVideoView.setScaleType(TextureVideoView.ScaleType.CENTER_CROP);
+        mTextureVideoView.setDataSource(LoginActivity.this, Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.hipchat));
+        mTextureVideoView.setLooping(true);
+
+        // Set up title
+        mLoginTitle = (TextView) findViewById(R.id.login_title);
+        Typeface font = Typeface.createFromAsset(getAssets(), "DIN Light.ttf");
+        mLoginTitle.setTypeface(font);
 
         // Set up the login form.
         mUsernameView = (EditText) findViewById(R.id.username_input);
@@ -58,7 +73,7 @@ public class LoginActivity extends Activity {
             }
         });
 
-        Button signInButton = (Button) findViewById(R.id.sign_in_button);
+        ImageView signInButton = (ImageView) findViewById(R.id.sign_in_button);
         signInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -67,6 +82,12 @@ public class LoginActivity extends Activity {
         });
 
         mSocket.on("login", onLogin);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mTextureVideoView.play();
     }
 
     @Override
